80127e1beb2f702bfdcb3e7e398d0fd1398abeda
Gleb Gubkin <glebgubckin@yandex.ru>
2022-12-27 16:34:15 +0300

    Setup tests

--- src/utils/profile.ts
+++ src/utils/profile.ts
@@ -11,8 +11,8 @@ export const updateProfile = (form: Form) => {
   localStorage.setItem('user', data)
 }
 
-export const updateValidate = (password1: string, password2: string) => {
-  if (password1.length === 0 && password2.length === 0) {
+export const updateValidate = (password1: string, password2: string): boolean => {
+  if (password1.length === 0 || password2.length === 0) {
     return false
   }
   return !(password1 === password2 && password1.length > 7 && password2.length > 7)
